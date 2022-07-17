5ee67138704552e7d0b20e5f94e3a6337ae7782b
Arseny Smorodin <arseny.smorodin@yandex.ru>
2022-07-17 21:37:56 +0300

    added products data and made render function

---
+++ js/renderCatalog.js
@@ -0,0 +1,30 @@
+getProductsData();
+
+async function getProductsData() {
+  const response = await fetch(`../js/products.json`);
+  const productsData = await response.json();
+
+  render(productsData);
+}
+
+function render(productsData) {
+  console.log(productsData);
+  let HTMLTemplate = ``;
+  productsData.forEach((product) => {
+    let cardTemplate = `
+      <div class="card">
+        <img src="${product.imgSrc}" class="card__img" alt="" height="500" width="300"/>
+        <span class="card__name">${product.name}</span>
+        <div class="card__bottom">
+          <span class="card__price">${product.price}</span>
+          <button class="card__button" id="${product.id}">
+            <i class="fi fi-br-shopping-cart-add"></i>
+          </button>
+        </div>
+      </div>`;
+    HTMLTemplate += cardTemplate;
+  });
+  const catalogEl = document.querySelector("#catalog");
+  console.log(catalogEl);
+  catalogEl.innerHTML = HTMLTemplate;
+}
